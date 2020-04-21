5fa0a41b56957126f6aaa5ac2dffa0ea0d470336
Frederik Mortensen <mortensen.frederik@gmail.com>
2020-04-21 23:01:12 +0200

    #11

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,24 +2,20 @@
  * Java jigsaw modularization
  */
 module scc {
-	
-	//needed exports
-	exports de.mortensenit to javafx.graphics;
-	exports de.mortensenit.gui to javafx.fxml;
-	exports de.mortensenit.model to javafx.fxml;
+
+	// needed exports
+	exports de.mortensenit;
+
+	opens de.mortensenit to javafx.fxml;
 	opens de.mortensenit.gui to javafx.fxml;
 
 	// javafx
-	requires transitive javafx.base;
-	requires transitive javafx.controls;
-	requires transitive javafx.fxml;
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
 	requires transitive javafx.graphics;
 
 	// log4j
-	requires org.apache.logging.log4j.core;
-	requires org.apache.logging.log4j;
-	requires org.apache.logging.log4j.slf4j;
-	requires junit;
 	requires org.slf4j;
 
 	// microstream
